8c8eefacce4fd7c5e383627a6766c11cb4ab5a03
Adrien_D <ghbdtn@openmailbox.org>
2020-01-24 07:35:05 +0100

    init script

---
+++ sauvegarde.sh
@@ -0,0 +1,94 @@
+#! /bin/bash
+
+# On se positionne au bon endroit"
+path=`dirname $0`
+cd "$path"
+
+# On défini les variables
+#gitrepo="git@github.com:aaaaadrien/gentoo-config.git"
+package="etc/portage/package."
+jour=$(date +"%d-%m-%Y")
+heure=$(date +"%H:%M:%S")
+
+# Début du script
+
+for fic in "use" "keywords" "mask" "unmask" "env"
+do
+	chemin="etc/portage/package.$fic/"
+	if [ -f "/$chemin/custom" ]
+	then
+		echo "On sauvegarde le fichier package.$fic ..."
+		
+		if [ -d $chemin ]
+		then
+			cp "/$chemin/custom" "$chemin/custom"
+		else
+			mkdir -p "$chemin"
+			cp "/$chemin/custom" "$chemin/custom"
+		fi
+	fi
+done
+
+
+chemin="etc/portage/env"
+fic="*"
+if [ -f "/$chemin/$fic" ]
+then
+        echo "On sauvegarde les fichiers perso env ..."
+        if [ -d $chemin ]
+        then
+                cp "/$chemin/$fic" "$chemin/$fic"
+        else
+                mkdir -p "$chemin"
+                cp "/$chemin/$fic" "$chemin/$fic"
+        fi
+fi
+
+chemin="etc/portage/"
+fic="make.conf"
+if [ -f "/$chemin/$fic" ]
+then
+        echo "On sauvegarde le $fic ..."
+        if [ -d $chemin ]
+        then
+                cp "/$chemin/$fic" "$chemin/$fic"
+        else
+                mkdir -p "$chemin"
+                cp "/$chemin/$fic" "$chemin/$fic"
+        fi
+fi
+
+chemin="var/lib/portage/"
+fic="world"
+if [ -f "/$chemin/$fic" ]
+then
+	echo "On sauvegarde la liste des applications installées..."
+	if [ -f $chemin/$fic ]
+	then
+		cp "/$chemin/$fic" "$chemin/$fic"
+	else
+		mkdir -p "$chemin"
+		cp "/$chemin/$fic" "$chemin/$fic"
+	fi
+fi
+
+
+chemin="usr/src/linux/"
+fic=".config"
+if [ -f "/$chemin/$fic" ]
+then
+	echo "On sauvegarde la conf kernel..."
+	if [ -f $chemin/$fic ]
+	then
+		cp "/$chemin/$fic" "$chemin/$fic"
+	else
+		mkdir -p "$chemin"
+		cp "/$chemin/$fic" "$chemin/$fic"
+	fi
+fi
+
+
+
+git add *
+git commit -m "Mise à jour de la configutaion de Gentoo le $jour à $heure"
+git push origin master
