a01d96c89ea94bba839765f764c9f7e54ececf70
Adrien_D <ghbdtn@openmailbox.org>
2020-01-25 00:13:53 +0100

    Mise à jour de la configutaion de Gentoo le 25-01-2020 à 00:13:53

--- sauvegarde.sh
+++ sauvegarde.sh
@@ -12,7 +12,7 @@ heure=$(date +"%H:%M:%S")
 
 # Début du script
 
-for fic in "use" "keywords" "mask" "unmask" "env"
+for fic in "use" "accept_keywords" "mask" "unmask" "env"
 do
 	chemin="etc/portage/package.$fic/"
 	if [ -f "/$chemin/custom" ]
